c6a7cf86abb68fc31cf0055fa980e8cf88f0c1db
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 12:29:33 +0200

    added IInputSystem interface

--- ExamSecondTry/ExamSecondTry/View/DisplayManager/Interfaces/IDisplay.cs
+++ ExamSecondTry/ExamSecondTry/View/DisplayManager/Interfaces/IDisplay.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExamSecondTry.View.Interfaces
+namespace ExamSecondTry.View.DisplayManager.Interfaces
 {
     public interface IDisplay<T> where T : class
     {
---
+++ ExamSecondTry/ExamSecondTry/View/InputManager/Interfaces/IInputSystem.cs
@@ -0,0 +1,8 @@
+﻿namespace ExamSecondTry.View.InputManager.Interfaces
+{
+    public interface IInputSystem
+    {
+        public string FetchStringValue(string messege);
+        public string FetchStringValueWithCondition(string messege, Predicate<string> contition);
+    }
+}
