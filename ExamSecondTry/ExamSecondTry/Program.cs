8dac7d8b4e0411f932d96703fc745ea84b343c68
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 12:10:02 +0200

    Created Solution

---
+++ ExamSecondTry/ExamSecondTry/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExamSecondTry
+{ 
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
