add3db89b3824591697d159db7feac5d990cd4e5
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 12:40:34 +0200

    implemented MovieDisplay class

---
+++ ExamSecondTry/ExamSecondTry/View/DisplayManager/MovieDisplay.cs
@@ -0,0 +1,18 @@
+﻿using ExamSecondTry.Data.Entities;
+using ExamSecondTry.View.DisplayManager.Interfaces;
+using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSecondTry.View.DisplayManager
+{
+    public class MovieDisplay : IDisplay<Movie>
+    {
+        public void DisplayList(List<Movie> entities) => entities.ForEach(e => DisplaySingle(e));
+
+        public void DisplaySingle(Movie entity) => WriteLine($"Title:\t{entity.Title}|Director\t{entity.Director}|Review:\t{entity.Review}|Length:\t{entity.Length}");
+    }
+}
