69c65c1d9a9e9b1b8c7374ef5ee121e7702dda44
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 13:09:43 +0200

    Implemented IBaseRepository Interface

---
+++ ExamSecondTry/ExamSecondTry.Data/DAL/Interfaces/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSecondTry.Data.DAL.Interfaces
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        public List<T> GetAll();
+        public T GetSingle(Func<T, bool> condition);
+        public void Add(T entity);
+        public void Edit(T entity);
+        public void Delete(T entity);
+        public void Sort();
+        public void Save();
+    }
+}
