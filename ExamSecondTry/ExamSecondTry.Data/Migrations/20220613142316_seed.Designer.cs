ba1c0ea3eb5d2f915cee1090b5199bdbdeacabde
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <jankes6463@gmail.com>
2022-06-13 16:25:07 +0200

    Added Seed migrations

--- ExamSecondTry/ExamSecondTry.Data/Migrations/20220613142316_seed.Designer.cs
+++ ExamSecondTry/ExamSecondTry.Data/Migrations/20220613142316_seed.Designer.cs
@@ -4,6 +4,7 @@ using ExamSecondTry.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExamSecondTry.Data.Migrations
 {
     [DbContext(typeof(MoviesContext))]
-    partial class MoviesContextModelSnapshot : ModelSnapshot
+    [Migration("20220613142316_seed")]
+    partial class seed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ExamSecondTry/ExamSecondTry.Data/Migrations/20220613142316_seed.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExamSecondTry.Data.Migrations
+{
+    public partial class seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Director", "Length", "Review", "Title" },
+                values: new object[] { new Guid("4518ad4b-28fc-489f-9fd6-ac25a4b620bd"), "Martin Scorsese", 3.0, "average", "The Wolf Of Wall Street" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Director", "Length", "Review", "Title" },
+                values: new object[] { new Guid("4a40dbfe-52f7-4d47-bfc1-7d7cb6146f2e"), "James Cameron", 3.1400000000000001, "Boring", "Titanic" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Director", "Length", "Review", "Title" },
+                values: new object[] { new Guid("a77d0dbe-033a-449b-b661-57be85bcc471"), "James Cameron", 2.3999999999999999, "Nice", "Avatar" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: new Guid("4518ad4b-28fc-489f-9fd6-ac25a4b620bd"));
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: new Guid("4a40dbfe-52f7-4d47-bfc1-7d7cb6146f2e"));
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: new Guid("a77d0dbe-033a-449b-b661-57be85bcc471"));
+        }
+    }
+}
