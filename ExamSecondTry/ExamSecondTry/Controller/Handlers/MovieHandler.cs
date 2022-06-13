670806d429f74ccd32746c93949dc8164aead437
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 13:22:34 +0200

    Implemented AddNew() method in MovieHandler class

--- ExamSecondTry/ExamSecondTry/Controller/Handlers/MovieHandler.cs
+++ ExamSecondTry/ExamSecondTry/Controller/Handlers/MovieHandler.cs
@@ -1,4 +1,8 @@
-﻿using ExamSecondTry.Data.DAL;
+﻿using ExamSecondTry.Controller.Factories.Interfaces;
+using ExamSecondTry.Data.DAL;
+using ExamSecondTry.Data.DAL.Interfaces;
+using ExamSecondTry.Data.Entities;
+using ExamSecondTry.View.DisplayManager.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +13,25 @@ namespace ExamSecondTry.Controller.Handlers
 {
     public class MovieHandler
     {
-        private MovieRepository movieRepository;
-        public MovieHandler()
+        private IBaseRepository<Movie> MovieRepository;
+        private IDisplay<Movie> MovieDisplay;
+        private IBaseFactory<Movie> MovieFactory;
+        public MovieHandler(IBaseRepository<Movie> movieRepository, IDisplay<Movie> movieDisplay, IBaseFactory<Movie> movieFactory)
         {
-            movieRepository = new();
+            MovieRepository = movieRepository;
+            MovieDisplay = movieDisplay;
+            MovieFactory = movieFactory;
         }
+
+        public void AddNew()
+        {
+            var newMovie = MovieFactory.Create();
+            MovieRepository.Add(newMovie);
+            MovieRepository.Save();
+        }
+
+        public void DisplayAll() => MovieDisplay.DisplayList(MovieRepository.GetAll());
+
+
     }
 }
