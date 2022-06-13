2ecb04a404cbf6bd75aa6706a08c5182086c4fa1
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 14:05:33 +0200

    Implemented Program Class

--- ExamSecondTry/ExamSecondTry/Program.cs
+++ ExamSecondTry/ExamSecondTry/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using ExamSecondTry.Controller.Factories;
+using ExamSecondTry.Controller.Handlers;
+using ExamSecondTry.Data.DAL;
+using ExamSecondTry.Data.DAL.Interfaces;
+using ExamSecondTry.Data.Entities;
+using ExamSecondTry.View.DisplayManager;
+using ExamSecondTry.View.DisplayManager.Interfaces;
+using ExamSecondTry.View.InputManager;
+using System;
 
 namespace ExamSecondTry
 { 
@@ -6,7 +14,14 @@ namespace ExamSecondTry
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MovieRepository movieRepository = new();
+            MovieDisplay movieDisplay = new();
+            InputManager inputSystem = new();
+            MovieFactory movieFactory = new(inputSystem); 
+            MovieHandler movieHandler = new(movieRepository, movieDisplay, movieFactory, inputSystem);
+            MenuDisplay menuDisplay = new();
+            AppHandler appHandler = new(movieHandler, menuDisplay, movieDisplay, inputSystem);
+            appHandler.Run();
         }
     }
 }
