45398d697a0fa8f9960043a443f40d64bfab3fdd
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 12:32:19 +0200

    implemented InputManager class

---
+++ ExamSecondTry/ExamSecondTry/View/InputManager/InputManager.cs
@@ -0,0 +1,25 @@
+﻿using ExamSecondTry.View.InputManager.Interfaces;
+using static System.Console;
+
+namespace ExamSecondTry.View.InputManager
+{
+    public class InputManager : IInputSystem
+    {
+        public string FetchStringValue(string messege)
+        {
+            WriteLine(messege);
+            return ReadLine();
+        }
+
+        public string FetchStringValueWithCondition(string messege, Predicate<string> contition)
+        {
+            string value = "";
+            do
+            {
+                WriteLine(messege);
+                value = ReadLine();
+            } while (!contition.Invoke(value));
+            return value;
+        }
+    }
+}
