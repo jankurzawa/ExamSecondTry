a82829c10b045f4eadac5898df7a930db423c85f
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 12:25:30 +0200

    added IMenuDisplay Interface

--- ExamSecondTry/ExamSecondTry/Controller/Factories/MovieFactory.cs
+++ ExamSecondTry/ExamSecondTry/Controller/Factories/MovieFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ExamSecondTry.Controller.Factories.Interfaces;
+using ExamSecondTry.Data.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace ExamSecondTry.Controller.Factories
 {
-    public class MovieFactory
+    public class MovieFactory : IBaseFactory<Movie>
     {
+        public Movie Create()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ ExamSecondTry/ExamSecondTry/View/Interfaces/IMenuDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSecondTry.View.Interfaces
+{
+    public interface IMenuDisplay
+    {
+        void ClearScreen();
+        void PrintMany<T>(List<T> entities);
+        void PrintMessage(string content);
+        void PrintOptions(List<string> options);
+    }
+}
