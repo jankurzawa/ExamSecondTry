c7facf2326f9cdcd3deea7c5ebdfb5cda162cb32
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 12:17:31 +0200

    Implemented MoviesContext class

---
+++ ExamSecondTry/ExamSecondTry.Data/Context/MoviesContext.cs
@@ -0,0 +1,24 @@
+﻿using ExamSecondTry.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSecondTry.Data.Context
+{
+    public class MoviesContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Movies;Integrated Security=True");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+    }
+}
