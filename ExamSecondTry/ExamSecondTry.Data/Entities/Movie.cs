3ebf9975cb2df560ecd775907e90bc7f673db96d
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 12:15:07 +0200

    Implemented Movie class

---
+++ ExamSecondTry/ExamSecondTry.Data/Entities/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSecondTry.Data.Entities
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Review { get; set; }
+        public double Length { get; set; }
+        public Movie()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Movie(string title, string direcotr, string review, double length)
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Director = direcotr;
+            Review = review;
+            Length = length;
+        }
+    }
+}
