de4fa4592c82ddd568f7c95ed5086dfc36d76552
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 12:20:13 +0200

    addet IBaseFactory Interface

---
+++ ExamSecondTry/ExamSecondTry/Controller/Factories/Interfaces/IBaseFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSecondTry.Controller.Factories.Interfaces
+{
+    public interface IBaseFactory<T>
+    {
+        public T Create();
+    }
+}
