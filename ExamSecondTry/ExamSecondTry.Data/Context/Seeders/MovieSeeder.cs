6a663cc39c6cb62654437cd1e77870faf12bf6fa
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <jankes6463@gmail.com>
2022-06-13 16:18:18 +0200

    Implemented MovieSeeder

---
+++ ExamSecondTry/ExamSecondTry.Data/Context/Seeders/MovieSeeder.cs
@@ -0,0 +1,24 @@
+﻿using ExamSecondTry.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSecondTry.Data.Context.Seeders
+{
+    public static class MovieSeeder
+    {
+        public static void SeedDatabase(this ModelBuilder builder)
+        {
+            List<Movie> movies = new();
+
+            movies.Add(new Movie("Titanic", "James Cameron", "Boring", 3.14));
+            movies.Add(new Movie("Avatar", "James Cameron", "Nice", 2.40));
+            movies.Add(new Movie("The Wolf Of Wall Street", "Martin Scorsese", "average", 3.00));
+            
+            builder.Entity<Movie>().HasData(movies);
+        }
+    }
+}
