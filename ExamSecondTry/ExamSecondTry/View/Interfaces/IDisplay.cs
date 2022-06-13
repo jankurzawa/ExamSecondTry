662e78dd156a088fecdac651c5fc58f3b2df89e7
Jan Kurzawa (CODECOOL POLAND SP. Z O.O.) <BFXV64@motorolasolutions.com>
2022-06-13 12:27:12 +0200

    added IDisplay interface

---
+++ ExamSecondTry/ExamSecondTry/View/Interfaces/IDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamSecondTry.View.Interfaces
+{
+    public interface IDisplay<T> where T : class
+    {
+        public void DisplaySingle(T entity);
+        public void DisplayList(List<T> entities);
+    }
+}
+}
